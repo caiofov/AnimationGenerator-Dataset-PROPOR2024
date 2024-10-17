6804389f5b479364e1fd742ecf8e59d2489c78d5
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-17 15:37:17 -0300

    feat: vite template

--- gh-page/src/lib/utils/text.ts
+++
@@ -1,4 +0,0 @@
-export function concatenateText(text: string, size: number | null, endChar = '...') {
-	if (size == null || text.length < size) return text;
-	return text.slice(0, size) + endChar;
-}
