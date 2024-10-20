14945baa7c5c2d22cdc2d63cc97287e3ff86c66d
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-20 13:09:33 -0300

    feat: call script on vite build

--- gh-page/vite.config.ts
+++ gh-page/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, PluginOption } from "vite";
+import react from "@vitejs/plugin-react";
+import { updateDatasetFile } from "./scripts/readImages";
+
+function runJsonScript() {
+  return {
+    name: "write-images",
+    buildStart() {
+      updateDatasetFile();
+    },
+  } as PluginOption;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), runJsonScript()],
+});
