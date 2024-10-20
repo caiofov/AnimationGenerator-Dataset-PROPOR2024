c8c736d5458046540895940be7fd5bb06694286e
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-20 15:10:22 -0300

    feat: check if there are changes before rewriting file - json script

--- gh-page/scripts/readImages.ts
+++ gh-page/scripts/readImages.ts
@@ -1,8 +1,16 @@
 import { readFileSync, readdirSync, writeFileSync } from "fs";
 import path from "path";
+
+function compareArrays(a: string[], b: string[]) {
+  return a.length === b.length
+    ? a.every((value, index) => value === b[index])
+    : false;
+}
+
 export async function updateDatasetFile() {
   const filePath = "src/assets/dataset/dataset.json";
   const imagePaths = "public/frames";
+  let update = false;
 
   try {
     console.info("Reading dataset json...");
@@ -19,11 +27,18 @@ export async function updateDatasetFile() {
           return Number(matchA[1]) - Number(matchB[1]);
         }
       );
-      json[folder]["images"] = images;
+      if (!compareArrays(images, json[folder]["images"])) {
+        json[folder]["images"] = images;
+        update = true;
+      }
     });
 
-    console.info("Updating dataset json file...");
-    writeFileSync(filePath, JSON.stringify(json), "utf-8");
+    if (update) {
+      console.info("Updating dataset json file...");
+      writeFileSync(filePath, JSON.stringify(json), "utf-8");
+    } else {
+      console.info("No changes");
+    }
   } catch (err) {
     console.error("Error on updating json file:", err);
   }
