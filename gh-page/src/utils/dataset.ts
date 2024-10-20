e9dd1d0a83426d05b9e5fd4fbc6759e27ecd127c
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-20 13:18:45 -0300

    feat: update types

--- gh-page/src/utils/dataset.ts
+++ gh-page/src/utils/dataset.ts
@@ -5,11 +5,24 @@ export interface DatasetType {
   id: IdType;
   prompt: string;
   generatedText: string;
+  images: string[];
 }
 export const DATASET_LIST: DatasetType[] = Object.entries(dataset).map(
-  ([key, { prompt, generatedText }]) => ({
+  ([key, { prompt, generatedText, images }]) => ({
     id: key as IdType,
     prompt,
     generatedText,
+    images,
   })
 );
+
+export type StoryIdType = "Ala" | "InJo" | "ViTe" | "ChVe" | "PePa" | "PePr";
+
+export const STORIES: { id: StoryIdType; name: string }[] = [
+  { id: "Ala", name: "Aladdin" },
+  { id: "InJo", name: "Indiana Jones" },
+  { id: "ViTe", name: "Journey to the Center of the Earth" },
+  { id: "ChVe", name: "Little Red Riding Hood" },
+  { id: "PePa", name: "Peter Pan" },
+  { id: "PePr", name: "The Little Prince" },
+];
