03b5ab5a5fe73f5328412d6020a5e36d614a6170
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-20 13:32:12 -0300

    feat: add carousel to cards

--- gh-page/src/components/DatasetDisplay.tsx
+++ gh-page/src/components/DatasetDisplay.tsx
@@ -1,40 +1,28 @@
 import React, { useReducer, useState } from "react";
-import { DATASET_LIST, DatasetType } from "../utils/dataset";
+import { DATASET_LIST, DatasetType, STORIES } from "../utils/dataset";
 import {
   Button,
   ButtonGroup,
   Card,
   CardContent,
-  CardMedia,
   Chip,
   Divider,
   Grid2,
   Typography,
 } from "@mui/material";
+import { Carousel } from "./Carousel";
 
-const buttons = [
-  { value: "", name: "All" },
-  { value: "Ala", name: "Aladdin" },
-  { value: "InJo", name: "Indiana Jones" },
-  { value: "ViTe", name: "Journey to the Center of the Earth" },
-  { value: "ChVe", name: "Little Red Riding Hood" },
-  { value: "PePa", name: "Peter Pan" },
-  { value: "PePr", name: "The Little Prince" },
-];
+const buttons = [{ id: "", name: "All" }, ...STORIES];
 
 const DatasetGrid: React.FC<{ dataset: DatasetType[] }> = ({ dataset }) => {
   return (
     <Grid2 container spacing={2}>
-      {dataset.map(({ id, prompt, generatedText }) => {
+      {dataset.map(({ id, prompt, generatedText, images }) => {
         return (
           <Grid2 size={4} key={id}>
             <Card key={id}>
-              <CardMedia
-                component="img"
-                height="100%"
-                src={`/src/assets/dataset/frames/${id}/${id}_frame10.png`}
-                alt={`${id}_frame0.png`}
-              />
+              <Carousel {...{ images, id }} />
+
               <Divider sx={{ marginTop: "15px" }}>
                 <Chip label={<Typography>{id}</Typography>} />
               </Divider>
@@ -68,12 +56,12 @@ export const DatasetDisplay = () => {
   return (
     <>
       <ButtonGroup>
-        {buttons.map(({ value, name }) => (
+        {buttons.map(({ id, name }) => (
           <Button
-            key={value}
-            variant={filter === value ? "contained" : "outlined"}
+            key={id}
+            variant={filter === id ? "contained" : "outlined"}
             onClick={() => {
-              dispatch({ filter: value });
+              dispatch({ filter: id });
             }}
           >
             {name}
