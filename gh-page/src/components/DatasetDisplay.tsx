bf510ade1c13debf677fcf2183f39593d2fc0406
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-20 17:29:59 -0300

    feat: organize card body

--- gh-page/src/components/DatasetDisplay.tsx
+++ gh-page/src/components/DatasetDisplay.tsx
@@ -1,66 +1,144 @@
 import React, { useReducer, useState } from "react";
 import { DATASET_LIST, DatasetType, STORIES } from "../utils/dataset";
 import {
+  Box,
   Button,
   ButtonGroup,
   Card,
+  CardActionArea,
   CardContent,
   Chip,
   Divider,
   Grid2,
   Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Tooltip,
   Typography,
 } from "@mui/material";
 import { Carousel } from "./Carousel";
-import SubjectIcon from "@mui/icons-material/Subject";
-const buttons = [{ id: "", name: "All" }, ...STORIES];
+
+// icons
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import ChatIcon from "@mui/icons-material/Chat";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+const buttons = [
+  { id: "", name: "All" },
+  ...Object.entries(STORIES).map(([k, v]) => ({ id: k, name: v })),
+];
+
+const itemIcons = {
+  Story: MenuBookIcon,
+  Generator: SmartToyIcon,
+  Prompt: ChatIcon,
+  "Generated text": LightbulbIcon,
+};
+
+const CardBodyItem: React.FC<{
+  title: keyof typeof itemIcons;
+  subtitle?: string;
+  text: string;
+}> = ({ title, text, subtitle }) => {
+  const Icon = itemIcons[title];
+  return (
+    <ListItem>
+      <ListItemIcon>
+        <Icon fontSize="small" />
+      </ListItemIcon>
+      <ListItemText sx={{ display: "flex" }}>
+        <Typography variant="subtitle2" component="h6">
+          {title}:
+        </Typography>
+        {subtitle ? <Typography variant="body2">{subtitle}</Typography> : null}
+
+        {text}
+      </ListItemText>
+    </ListItem>
+  );
+};
 
 const DatasetGrid: React.FC<{ dataset: DatasetType[] }> = ({ dataset }) => {
   return (
     <Grid2 container spacing={2}>
-      {dataset.map(({ id, prompt, generatedText, images, map }) => {
-        return (
-          <Grid2 size={4} key={id}>
-            <Card key={id}>
-              {images.length ? (
-                <Carousel {...{ images, id }} />
-              ) : (
-                <Typography>
-                  Something went wrong with this animation
-                </Typography>
-              )}
-
-              <Divider sx={{ marginTop: "15px" }}>
-                <Chip label={<Typography>{id}</Typography>} />
-              </Divider>
-              <CardContent>
-                <Divider sx={{ marginTop: "15px" }} textAlign="left">
-                  Prompt
-                </Divider>
-                {prompt}
-                <Divider sx={{ marginTop: "15px" }} textAlign="left">
-                  Generated text
-                </Divider>
-                {generatedText}
-              </CardContent>
-
-              {map ? (
-                <Tooltip title="See map JSON on GitHub">
-                  <Link
-                    href={`https://github.com/caiofov/AnimationGenerator-Dataset-PROPOR2024/blob/main/Animations/data/${map}`}
-                    target="_blank"
-                    underline="none"
-                    variant="body2"
+      {dataset.map(
+        ({
+          id,
+          storyID,
+          prompt,
+          generatedText,
+          images,
+          map,
+          generator,
+          promptNumber,
+          resultNumber,
+        }) => {
+          return (
+            <Grid2 size={4} key={id}>
+              <Card key={id} sx={{ height: "100%" }}>
+                {images.length ? (
+                  <Carousel {...{ images, id }} />
+                ) : (
+                  <Box
+                    sx={{ width: "100%", height: "41%" }}
+                    alignItems="center"
+                    justifyContent="center"
+                    display="flex"
+                    flexDirection="column"
                   >
-                    <SubjectIcon />
-                  </Link>
-                </Tooltip>
-              ) : null}
-            </Card>
-          </Grid2>
-        );
-      })}
+                    <SentimentVeryDissatisfiedIcon />
+                    Something went wrong with this animation
+                  </Box>
+                )}
+
+                <Divider>
+                  <Chip
+                    label={
+                      <Typography variant="overline" component="h5">
+                        {id}
+                      </Typography>
+                    }
+                  />
+                </Divider>
+                <CardContent>
+                  <List>
+                    <CardBodyItem title="Story" text={STORIES[storyID]} />
+                    <CardBodyItem title="Generator" text={generator} />
+                    <CardBodyItem
+                      title="Prompt"
+                      subtitle={`(nº ${promptNumber})`}
+                      text={prompt}
+                    />
+                    <CardBodyItem
+                      title="Generated text"
+                      subtitle={`(nº ${resultNumber})`}
+                      text={generatedText}
+                    />
+                  </List>
+
+                  {map ? (
+                    <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                      <Tooltip title="See map JSON on GitHub">
+                        <Link
+                          href={`https://github.com/caiofov/AnimationGenerator-Dataset-PROPOR2024/blob/main/Animations/data/${map}`}
+                          target="_blank"
+                          underline="none"
+                        >
+                          <GitHubIcon />
+                        </Link>
+                      </Tooltip>
+                    </Box>
+                  ) : null}
+                </CardContent>
+              </Card>
+            </Grid2>
+          );
+        }
+      )}
     </Grid2>
   );
 };
