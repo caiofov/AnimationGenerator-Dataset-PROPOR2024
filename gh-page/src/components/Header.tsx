4f8038ed08bbff84262a3729050c41304b56acc9
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-20 23:37:44 -0300

    feat: add authors to header

--- gh-page/src/components/Header.tsx
+++ gh-page/src/components/Header.tsx
@@ -6,33 +6,39 @@ export const Header = () => {
   return (
     <Paper
       sx={{
+        padding: "20px",
+        marginBottom: "5%",
         display: "flex",
         alignItems: "center",
         gap: "1%",
-        padding: "20px",
-        marginBottom: "5%",
       }}
     >
       <Box width="30%" display="flex" alignItems="center" gap="10%">
         <Box
           component="img"
           alt="Logo UFC"
-          title="UFC"
+          title="Federal University of Ceará"
           height="100px"
           src="https://www.ufc.br/images/_images/a_universidade/identidade_visual/brasao/brasao2_vertical_cor_300dpi.png"
         />
         <Box
           component="img"
           alt="Logo PROPOR"
-          title="Propor"
+          title="PROPOR"
           height="100px"
           src="https://propor2024.citius.gal/wp-content/uploads/2024/02/logoPropor.png"
         />
       </Box>
-      <Typography variant="h1" component="h1" fontSize="3rem">
-        Dataset - A Natural Language Text to Role-Playing Game Animation
-        Generator
-      </Typography>
+      <Box gap="10px" display="flex" flexDirection="column">
+        <Typography variant="h1" component="h1" fontSize="3rem">
+          Dataset - A Natural Language Text to Role-Playing Game Animation
+          Generator
+        </Typography>
+        <Typography variant="subtitle1">
+          Caio de F. Oliveira, Artur O. R. Franco, Wellington Franco and José G.
+          R. Maia
+        </Typography>
+      </Box>
     </Paper>
   );
 };
